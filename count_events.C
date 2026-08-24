667da70db1b271fae3cc8a99c839e4e6c3a942fd
Charlotte Knight <mdk16@ic.ac.uk>
2026-08-24 15:51:09 +0100

    it should work now

--- count_events.C
+++ count_events.C
@@ -3,31 +3,9 @@
 void count_events() {
     // Use the cernstaff.root tutorial file shipped with ROOT itself.
     // gROOT->GetTutorialDir() gives the correct path regardless of ROOT version.
-    TString filename = TString(gROOT->GetTutorialDir()) + "/tree/cernstaff.root";
-    const char *treename = "T";
-    const char *outfile  = "output.txt";
+    TString filename = TString(gROOT->GetTutorialDir()) + "/hsimple.root";
 
-    TFile *f = TFile::Open(filename.Data());
-    if (!f || f->IsZombie()) {
-        fprintf(stderr, "ERROR: Cannot open %s\n", filename.Data());
-        exit(1);
-    }
-    TTree *t = (TTree *)f->Get(treename);
-    if (!t) {
-        fprintf(stderr, "ERROR: Cannot find tree '%s' in %s\n", treename, filename.Data());
-        f->Close();
-        exit(1);
-    }
-    Long64_t n = t->GetEntries();
-    f->Close();
-
-    FILE *out = fopen(outfile, "w");
-    if (!out) {
-        fprintf(stderr, "ERROR: Cannot write %s\n", outfile);
-        exit(1);
-    }
-    fprintf(out, "%lld\n", n);
-    fclose(out);
-
-    printf("Number of entries: %lld\n", n);
+    auto rdf = ROOT::RDataFrame("ntuple", filename);
+    int count = rdf.Count().GetValue();    
+    std::cout << count << std::endl;
 }
